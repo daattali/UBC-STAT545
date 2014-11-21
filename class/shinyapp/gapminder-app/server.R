908112776ea6ba700b9015be4960943658ded033
Dean Attali <daattali@gmail.com>
2014-11-20 21:48:52 -0800

    update class shiny app

--- class/shinyapp/gapminder-app/server.R
+++ class/shinyapp/gapminder-app/server.R
@@ -1,19 +1,39 @@
+library(ggplot2)
+
 gdURL <- "http://tiny.cc/gapminder"
 gDat <- read.delim(file = gdURL) 
 
 shinyServer(function(input, output) {
+
+	one_country_data  <- reactive({
+		cat("country:\n")
+		cat(input$select_country)
+		if(is.null(input$select_country)) {
+			return(NULL)
+		}				
+	  subset(gDat, country == input$select_country &
+	  			 	     year >= input$year_range[1] & year <= input$year_range[2] )
+	})
 	
 	output$gapminder_table <- renderTable({ 
-		subset(gDat, country == input$select_country & year >= input$year_range[1] &
-					 	year <= input$year_range[2])
+		cat("render table\n")
+		one_country_data()
 	})
 	output$output_country <- renderText({
-		paste("Country selected", input$select_country)
+		paste("Country: ", input$select_country)
 	})
 	output$country_select_ui <- renderUI({
 		selectInput("select_country", 
 								label = "Country",
 								choices = levels(gDat$country)
 		)
+	})
+	output$ggplot_gdp_vs_country <- renderPlot({
+		cat("render plot\n")
+		if(is.null(one_country_data())) {
+			return(NULL)
+		}		
+		p <- ggplot(one_country_data(), aes(x = year, y = gdpPercap))
+		p + geom_point()
 	})
 })
--- class/shinyapp/gapminder-app/ui.R
+++ class/shinyapp/gapminder-app/ui.R
@@ -3,15 +3,19 @@ shinyUI(fluidPage(
 	
 	sidebarLayout(
 		sidebarPanel(
+			h2("What country and years do you want to see?"),
 								 uiOutput("country_select_ui"),
 								 sliderInput("year_range", 
 								 						label = "Range of years:",
-								 						min = 1952, max = 2007, value = c(1955, 2005),
+								 						min = 1952, max = 2007,
+								 						value = c(1955, 2005),
+								 						format = "####",
 								 						step = 5)
 		),
 		mainPanel(
-							textOutput("output_country"),
-							tableOutput("gapminder_table")
+							h3(textOutput("output_country"), align = "center"),
+							tableOutput("gapminder_table"),
+							plotOutput("ggplot_gdp_vs_country")
 		)
 	)
 ))
